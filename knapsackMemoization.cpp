5f9a1e96180cdfedc596c0551b78fb694ae55c12
why-deepanshux <deepanshusharma12800@gmail.com>
2025-01-22 21:52:27 +0530

    memoization code

--- knapsackMemoization.cpp
+++ knapsackMemoization.cpp
@@ -4,23 +4,26 @@
 using namespace std;
 
 
-int knapsack(vector<int>& weight, vector<int>& value, int n, int W) {
+int knapsack(vector<int>& weight, vector<int>& value, int n, int W ,vector<vector<int>>&dp) {
     // Base condition
     if (n == 0 || W == 0) {
         return 0;
     }
 
+    if(dp[n][W]!=-1)return dp[n][W];
+
+
     // Choice diagram
     if (weight[n - 1] <= W) {
         // Include the item or skip it
-        return max(
-            value[n - 1] + knapsack(weight, value, n - 1, W - weight[n - 1]),
-            knapsack(weight, value, n - 1, W)
+        return dp[n][W]=max(
+            value[n - 1] + knapsack(weight, value, n - 1, W - weight[n - 1],dp),
+            knapsack(weight, value, n - 1, W,dp)
         );
     }
 
     // Skip the item
-    return knapsack(weight, value, n - 1, W);
+    return dp[n][W]=knapsack(weight, value, n - 1, W,dp);
 }
 
 int main() {
@@ -34,9 +37,10 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> weight[i] >> value[i];
     }
-    
+    // make a matrix to memoize the problem
+    vector<vector<int>> dp(n+1, vector<int>(W+1,-1));
     // Printing maximum profit
-    int maxProfit = knapsack(weight, value, n, W);
+    int maxProfit = knapsack(weight, value, n, W,dp);
     cout << maxProfit;
 
     return 0;
